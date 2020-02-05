b31d71761ef63e7b29adf738d6210cec7afd373b
Nick <nshupinski@gmail.com>
2020-02-04 20:35:31 -0500

    A button was added to change the text view by using event listener

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,14 +3,35 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+
+Button btnClickMe;
+TextView txt;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnClickMe = (Button) findViewById(R.id.btnClickMe);
+        txt = (TextView) findViewById(R.id.txt);
+
+
+        btnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                txt.setText("You have clicked the button");
+            }
+        });
     }
 }
